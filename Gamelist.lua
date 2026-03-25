9323a489145ba7b521733e8f8b0b6d617c4a70eb
Muhammad Aldi <106606027+indomieee@users.noreply.github.com>
2026-03-26 03:43:46 +0700

    add game list

---
+++ Gamelist.lua
@@ -0,0 +1,6 @@
+local Games = {
+    [75528440071743]  = "https://raw.githubusercontent.com/indomieee/list-game/refs/heads/main/BWFB.lua",
+    [119987266683883] = "https://raw.githubusercontent.com/indomieee/list-game/refs/heads/main/SLFB.lua",
+}
+
+return Games
