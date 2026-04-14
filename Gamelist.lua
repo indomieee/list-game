33d9407137db78329f9fd36bc116e2057963a19d
Muhammad Aldi <106606027+indomieee@users.noreply.github.com>
2026-04-14 13:48:56 +0700

    ADDED NEW GAME

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,6 +1,7 @@
 local Games = {
     [75528440071743]  = "https://raw.githubusercontent.com/indomieee/list-game/refs/heads/main/BWFB.lua",
     [119987266683883] = "https://raw.githubusercontent.com/indomieee/list-game/refs/heads/main/SLFB.lua",
+    [124473577469410] = "https://raw.githubusercontent.com/indomieee/list-game/refs/heads/main/BALB.lua",
 }
 
 return Games
