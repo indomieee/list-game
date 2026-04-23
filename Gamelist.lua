655dd47a451a1d8df9b1fadf7ceee559f29654e4
Muhammad Aldi <106606027+indomieee@users.noreply.github.com>
2026-04-24 03:29:02 +0700

    ADDED NEW GAME
    Added a new game entry for KALB.

--- Gamelist.lua
+++ Gamelist.lua
@@ -3,6 +3,7 @@ local Games = {
     [119987266683883] = "https://raw.githubusercontent.com/indomieee/list-game/refs/heads/main/SLFB.lua",
     [124473577469410] = "https://raw.githubusercontent.com/indomieee/list-game/refs/heads/main/BALB.lua",
     [136919941417380] = "https://raw.githubusercontent.com/indomieee/list-game/refs/heads/main/BOFB.lua",
+    [89469502395769] = "https://raw.githubusercontent.com/indomieee/list-game/refs/heads/main/KALB.lua",
 }
 
 return Games
